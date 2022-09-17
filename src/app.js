724b50b28aba0c6e2c69e6081a9aee34e8e21fbe
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-17 01:01:01 -0400

    install handlebars for express package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const publicDirPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
+app.use(express.static(publicDirPath))
+
+
+
+
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'New York'
+    })
 })
-
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
