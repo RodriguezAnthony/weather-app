d11137a86068228a08e832048849aa598b3aa9d0
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-17 01:25:19 -0400

    Create routes to handlebars

--- src/app.js
+++ src/app.js
@@ -7,9 +7,27 @@ const publicDirPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirPath))
 
 
+app.set('view engine', 'hbs')
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Anthony R'
+    })
+})
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Anthony R'
+    })
+})
 
+app.get('/help', (req, res) =>{
+    res.render('help', {
+        helpText: 'Some helpful text'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
