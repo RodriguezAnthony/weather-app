2f1e3f9947638a9728ff65eba0c2e01bf3a4c64a
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-21 00:43:18 -0400

    set up our views path to point to it in our project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
+
+
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+
 
 app.use(express.static(publicDirPath))
 
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.get('', (req, res) => {
     res.render('index', {
